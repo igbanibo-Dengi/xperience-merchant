2eae40d641192901cdafe719bafaeb3958ffe7cd
igbanibo-Dengi <igbanibodengi@gmail.com>
2025-02-24 10:12:50 +0100

    created plans server actions

--- app/(root)/pricing/page.tsx
+++ app/(root)/pricing/page.tsx
@@ -1,10 +1,11 @@
 import { PaymentFlow } from '@/components/forms/payment-flow'
-import { getAllPlans } from '@/lib/actions/plans/plans.actions'
+import { getAllPlans, getUserPlan } from '@/lib/actions/plans/plans.actions'
 
 export default async function Page() {
-  const plans = await getAllPlans()
+  // const plans = await getAllPlans()
+  // const myPlan = await getUserPlan()
 
-  // console.log(plans);
+  // console.log(myPlan);
 
   return (
     <div className="py-10">
--- lib/actions/plans/plans.actions.ts
+++ lib/actions/plans/plans.actions.ts
@@ -5,10 +5,8 @@ import { NextResponse } from 'next/server'
 
 export async function getAllPlans() {
   try {
-    // Use a server-side environment variable instead of NEXT_PUBLIC_
     const url = `${process.env.BASE_URL}/plan`
 
-    // Use await with cookies() as it's now asynchronous
     const token = (await cookies()).get('auth_token')?.value
 
     if (!token) {
@@ -31,6 +29,44 @@ export async function getAllPlans() {
     const data = await response.json()
 
     return data
+  } catch (error) {
+    console.error('Error in get plans action:', error)
+    return NextResponse.json(
+      { message: 'Failed to fetch data' },
+      { status: 500 }
+    )
+  }
+}
+
+
+export async function getUserPlan() {
+
+  try {
+    const url = `${process.env.BASE_URL}/plan/user`
+
+    const token = (await cookies()).get('auth_token')?.value
+
+    if (!token) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+    }
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch data: ${response.status} ${response.statusText}`
+      )
+    }
+
+    const data = response.json()
+
+    return data
+
   } catch (error) {
     console.error('Error in get plans action:', error)
     return NextResponse.json(
